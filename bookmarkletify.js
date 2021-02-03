c7c87057d2d1fef45defa4c81d7e0cd2c4bfe289
David Kellner <52860029+kellnerd@users.noreply.github.com>
2021-02-03 13:09:14 +0100

    Create a Node.js script to compile bookmarklets
    - Load the JS file which is given as script argument
    - Convert the contained function to an IIFE
    - Minify code with UglifyJS and output it as javascript:(code)() URI

---
+++ bookmarkletify.js
@@ -0,0 +1,30 @@
+const UglifyJS = require('uglify-js');
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+	process.exit(1)
+};
+
+const codeFile = process.argv[2];
+
+try {
+	const code = fs.readFileSync(codeFile, { encoding: 'utf-8' });
+	const result = UglifyJS.minify(`(${code})()`, {
+		compress: {
+			expression: true, // preserve completion values from terminal statements without return, e.g. in bookmarklets
+			drop_console: true,
+		},
+		output: {
+			ascii_only: true,
+			quote_style: 1, // always use single quotes
+		}
+	});
+	if (result.error) {
+		console.error(result.error);
+	} else {
+		const bookmarklet = `javascript:${result.code}`;
+		console.log(bookmarklet);
+	}
+} catch (error) {
+	console.error(error.message);
+}
