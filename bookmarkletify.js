84be2b736c5911da9c505928cf3c708d775df721
David Kellner <52860029+kellnerd@users.noreply.github.com>
2021-02-03 18:07:22 +0100

    Refactor input transformation into its own function
    - Add inline documentation comments to Unicode guess script
    - Adapt bookmarklet to launch the function the JS file is named after

--- bookmarkletify.js
+++ bookmarkletify.js
@@ -1,15 +1,17 @@
 const UglifyJS = require('uglify-js');
 const fs = require('fs');
+const path = require('path');
 
 if (process.argv.length < 3) {
 	process.exit(1)
 };
 
 const codeFile = process.argv[2];
+const scriptName = path.basename(codeFile, '.js');
 
 try {
 	const code = fs.readFileSync(codeFile, { encoding: 'utf-8' });
-	const result = UglifyJS.minify(`(${code})()`, {
+	const result = UglifyJS.minify(`(()=>{${scriptName}();${code}})()`, {
 		compress: {
 			expression: true, // preserve completion values from terminal statements without return, e.g. in bookmarklets
 			drop_console: true,
--- src/guessUnicodePunctuation.js
+++ src/guessUnicodePunctuation.js
@@ -1,3 +1,28 @@
+/**
+ * Transforms the values of the selected input fields using the given substitution rules.
+ * @param {string} inputSelector CSS selector of the input fields.
+ * @param {(string|RegExp)[][]} substitutionRules Pairs of values for search & replace.
+ */
+function transformInputValues(inputSelector, substitutionRules) {
+	$(inputSelector).css('background-color', ''); // disable possible previously highlighted changes
+	$(inputSelector).each((_index, input) => {
+		let value = input.value;
+		substitutionRules.forEach(([searchValue, newValue]) => {
+			value = value.replace(searchValue, newValue);
+			console.debug(value);
+		});
+		if (value != input.value) { // update and highlight changed values
+			$(input).val(value)
+				.trigger('change')
+				.css('background-color', 'yellow');
+		}
+	});
+}
+
+/**
+ * Searches and replaces ASCII punctuation symbols for all title input fields by their preferred Unicode counterparts.
+ * These can only be guessed based on context as the ASCII symbols are ambiguous.
+ */
 function guessUnicodePunctuation() {
 	const titleInputSelectors = [
 		'input#name', // release title (release editor)
@@ -5,7 +30,7 @@ function guessUnicodePunctuation() {
 		'#id-edit-recording\\.name', // recording name (recording editor)
 		'#id-edit-work\\.name', // work name (work editor)
 	];
-	let transformationRules = [
+	const transformationRules = [
 		[/(?<!\S)"(.+?)"(?!\S)/g, '“$1”'], // double quotes
 		[/(?<!\S)'(.+?)'(?!\S)/g, '‘$1’'], // single quotes
 		// ... which are not following / followed by non-whitespace characters
@@ -14,21 +39,10 @@ function guessUnicodePunctuation() {
 		[/'/g, '’'], // ... and finally the apostrophes should be remaining
 		[/(?<!\.)\.{3}(?!\.)/g, '…'], // horizontal ellipsis (but not more than three dots)
 		[/ - /g, ' – '], // en dash
-		[/(\d+)-(\d+)/g, '$1–$2'], // en dash for spans where it means "to", e.g. 1965–1972
+		[/(\d+)-(\d+)/g, '$1–$2'], // en dash for ranges where it means "to", e.g. 1965–1972
 		[/-/g, '‐'], // hyphen
 		// difficult to find rules for: em dash (rare), minus (very rare), figure dash (very rare)
 		// TODO: localize quotes using release/lyrics language
 	];
-
-	$(titleInputSelectors.join()).each((_index, input) => {
-		let value = input.value;
-		$(input).css('background-color', '');
-		transformationRules.forEach(([searchValue, newValue]) => {
-			value = value.replace(searchValue, newValue);
-			console.debug(value);
-		});
-		if (value != input.value) {
-			$(input).val(value).trigger('change').css('background-color', 'yellow');
-		}
-	});
+	transformInputValues(titleInputSelectors.join(), transformationRules);
 }
