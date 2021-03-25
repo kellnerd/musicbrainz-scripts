7d9bf9d7851f3ecfee8f9c7b63fc50f04dbb8e49
David Kellner <52860029+kellnerd@users.noreply.github.com>
2021-03-25 18:23:37 +0100

    annotation: Refine description

--- src/bookmarklets/annotationConverter.js
+++ src/bookmarklets/annotationConverter.js
@@ -1,5 +1,5 @@
 /**
- * - Allows annotations to be partly written in basic Markdown and converts them into valid annotation markup.
+ * - Allows entity annotations to be (partly) written in basic Markdown and converts them into valid annotation markup.
  * - Shortens absolute URLs to MusicBrainz entities to `[entity-type:mbid|label]` links.
  * - Automatically fetches and uses the name of the linked entity as label if none was given.
  */
