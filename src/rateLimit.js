f5edec03c1d8e209ae0ca76928722c68cee19b8c
David Kellner <52860029+kellnerd@users.noreply.github.com>
2021-06-20 18:52:38 +0200

    Implement a rate limiting utility

---
+++ src/rateLimit.js
@@ -0,0 +1,32 @@
+// Adapted from https://thoughtspile.github.io/2018/07/07/rate-limit-promises/
+
+/**
+ * Returns a promise that resolves after the given delay.
+ * @param {number} ms Delay in milliseconds.
+ */
+const delay = ms => new Promise((resolve, reject) => setTimeout(resolve, ms));
+
+function rateLimit1(operation, interval) {
+	let queue = Promise.resolve(); // empty queue is ready
+	return (...args) => {
+		const result = queue.then(() => operation(...args)); // queue the next operation
+		queue = queue.then(() => delay(interval)); // start the next delay
+		return result;
+	};
+}
+
+/**
+ * Limits the number of requests for the given operation within a time interval.
+ * @param {(...args)=>any} operation Operation that should be rate-limited.
+ * @param {number} interval Time interval (in ms).
+ * @param {*} requestsPerInterval Maximum number of requests within the interval.
+ * @returns {(...args)=>Promise<any>} Rate-limited version of the given operation.
+ */
+export function rateLimit(operation, interval, requestsPerInterval = 1) {
+	const queues = Array(requestsPerInterval).fill().map(() => rateLimit1(operation, interval));
+	let queueIndex = 0;
+	return (...args) => {
+		queueIndex = (queueIndex + 1) % requestsPerInterval; // use the next queue
+		return queues[queueIndex](...args); // return the rate-limited operation
+	};
+}
