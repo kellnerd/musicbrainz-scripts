070fd22bdc95e22aaa1d760d64d15a01c9cc1172
David Kellner <52860029+kellnerd@users.noreply.github.com>
2021-06-21 11:10:34 +0200

    annotation: Support collection descriptions

--- src/bookmarklets/annotationConverter.js
+++ src/bookmarklets/annotationConverter.js
@@ -1,5 +1,6 @@
 /**
- * - Allows entity annotations to be (partly) written in basic Markdown and converts them into valid annotation markup.
+ * - Allows entity annotations and collection descriptions to be (partly) written in basic Markdown and converts them
+ *   into valid annotation markup.
  * - Shortens absolute URLs to MusicBrainz entities to `[entity-type:mbid|label]` links.
  * - Automatically fetches and uses the name of the linked entity as label if none was given.
  */
@@ -10,7 +11,10 @@ import {
 } from '../annotationConverter.js';
 import { transformInputValues } from '../transformInputValues.js';
 
-const annotationInput = 'textarea[name$=text]';
+const annotationInput = [
+	'textarea[name$=text]', // entity annotation
+	'textarea[name$=description]', // collection description
+].join();
 
 transformInputValues(annotationInput, markdownToAnnotation);
 
