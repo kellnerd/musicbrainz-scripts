0a97b63c27385144adc658f235edef6e989b6d54
David Kellner <52860029+kellnerd@users.noreply.github.com>
2021-07-01 13:27:55 +0200

    Optimize rate limiter
    - Omit the queue index overhead for the special case of 1 request/interval
    - Improve IntelliSense for the resulting function by using template types

--- src/rateLimit.js
+++ src/rateLimit.js
@@ -17,12 +17,17 @@ function rateLimit1(operation, interval) {
 
 /**
  * Limits the number of requests for the given operation within a time interval.
- * @param {(...args)=>any} operation Operation that should be rate-limited.
+ * @template Params
+ * @template Result
+ * @param {(...args:Params)=>Result} operation Operation that should be rate-limited.
  * @param {number} interval Time interval (in ms).
- * @param {*} requestsPerInterval Maximum number of requests within the interval.
- * @returns {(...args)=>Promise<any>} Rate-limited version of the given operation.
+ * @param {number} requestsPerInterval Maximum number of requests within the interval.
+ * @returns {(...args:Params)=>Promise<Result>} Rate-limited version of the given operation.
  */
 export function rateLimit(operation, interval, requestsPerInterval = 1) {
+	if (requestsPerInterval == 1) {
+		return rateLimit1(operation, interval);
+	}
 	const queues = Array(requestsPerInterval).fill().map(() => rateLimit1(operation, interval));
 	let queueIndex = 0;
 	return (...args) => {
