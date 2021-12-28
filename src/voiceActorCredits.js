74442d1db0cfff3df3e9040adca798c2f8848cad
David Kellner <52860029+kellnerd@users.noreply.github.com>
2021-12-28 21:43:24 +0100

    voice-actor: Improve log messages and level

--- src/voiceActorCredits.js
+++ src/voiceActorCredits.js
@@ -17,7 +17,7 @@ import {
 export async function importVoiceActorsFromDiscogs(releaseURL, event = document.createEvent('MouseEvent')) {
 	const actors = await fetchVoiceActorsFromDiscogs(releaseURL);
 	for (const actor of actors) {
-		console.info(actor);
+		console.debug(actor);
 		const roleName = actor.roleCredit;
 		const artistCredit = actor.anv || actor.name; // ANV is empty if it is the same as the main name
 		const mbArtist = await getEntityForResourceURL('artist', buildDiscogsURL('artist', actor.id));
@@ -29,7 +29,7 @@ export async function importVoiceActorsFromDiscogs(releaseURL, event = document.
 			createVoiceActorDialog(internalArtist(mbArtist), roleName, artistCredit).accept();
 			// TODO: catch exception which occurs for duplicate rels
 		} else {
-			console.warn(`Failed to add credit '${roleName}' for '${actor.name}' => Guessing...`);
+			console.info('Failed to find the linked MB artist for:', actor);
 			const mbArtistGuess = (await searchEntity('artist', actor.name))[0]; // first result
 			const dialog = createVoiceActorDialog(mbArtistGuess, roleName, artistCredit);
 
