9a4fb748f7b8ace7cbc76e2ff0a2093db6b49cfc
David Kellner <52860029+kellnerd@users.noreply.github.com>
2021-12-29 12:06:54 +0100

    Define types for the relationship editor dialogs

--- src/relationshipEditor.js
+++ src/relationshipEditor.js
@@ -5,7 +5,7 @@ import {
 
 /**
  * Creates a dialog to add a relationship to the currently edited source entity.
- * @param {{gid:string,name:string}} targetEntity Target entity of the relationship.
+ * @param {TargetEntity} targetEntity Target entity of the relationship.
  * @returns Pre-filled "Add relationship" dialog object.
  */
 export function createAddRelationshipDialog(targetEntity) {
@@ -20,19 +20,21 @@ export function createAddRelationshipDialog(targetEntity) {
 /**
  * Creates an "Add relationship" dialogue where the type "vocals" and the attribute "spoken vocals" are pre-selected.
  * Optionally the performing artist (voice actor) and the name of the role can be pre-filled.
- * @param {Object} artistData Edit data of the performing artist (optional).
- * @param {string} roleName Credited name of the voice actor's role (optional).
- * @param {string} artistCredit Credited name of the performing artist (optional).
+ * @param {InternalEntity} [artistData] Edit data of the performing artist (optional).
+ * @param {string} [roleName] Credited name of the voice actor's role (optional).
+ * @param {string} [artistCredit] Credited name of the performing artist (optional).
  * @returns MusicBrainz "Add relationship" dialog.
  */
 export function createVoiceActorDialog(artistData = {}, roleName = '', artistCredit = '') {
 	const viewModel = MB.releaseRelationshipEditor;
-	let target = new MB.entity(artistData, 'artist'); // automatically caches entities (unlike `MB.entity.Artist`)
+	/** @type {TargetEntity} */
+	const target = new MB.entity(artistData, 'artist'); // automatically caches entities (unlike `MB.entity.Artist`)
 	const dialog = new MB.relationshipEditor.UI.AddDialog({
 		source: viewModel.source,
 		target,
 		viewModel,
 	});
+
 	const rel = dialog.relationship();
 	rel.linkTypeID(60); // set type: performance -> performer -> vocals
 	rel.entity0_credit(artistCredit);
@@ -40,13 +42,14 @@ export function createVoiceActorDialog(artistData = {}, roleName = '', artistCre
 		type: { gid: 'd3a36e62-a7c4-4eb9-839f-adfebe87ac12' }, // spoken vocals
 		credited_as: roleName,
 	}]);
+
 	return dialog;
 }
 
 /**
  * Creates an entity object which can be used as target of relationships.
  * @param {string} url URL of a MusicBrainz entity page.
- * @returns {Promise<{gid:string,name:string}>}
+ * @returns {Promise<TargetEntity>}
  */
 export async function targetEntityFromURL(url) {
 	const entity = extractEntityFromURL(url);
@@ -73,10 +76,23 @@ export function ensureNoActiveDialog(editor = MB.releaseRelationshipEditor) {
 /**
  * Opens the given dialog, focuses the autocomplete input and triggers the search.
  * @param {*} dialog 
- * @param {Event} event Affects the position of the opened dialog.
+ * @param {Event} [event] Affects the position of the opened dialog (optional).
  */
 export function openDialogAndTriggerAutocomplete(dialog, event = document.createEvent('MouseEvent')) {
 	dialog.open(event);
 	dialog.autocomplete.$input.focus();
 	dialog.autocomplete.search();
 }
+
+/**
+ * @typedef InternalEntity Internal entity object, can be an empty record or just a name.
+ * @property {string} [name] Name of the entity.
+ * @property {string} [gid] MBID of the entity.
+ */
+
+/**
+ * @typedef TargetEntity Entity object which can be used as target of relationships.
+ * @property {string} entityType Type of the entity.
+ * @property {string} name Name of the entity.
+ * @property {string} [gid] MBID of the entity.
+ */
