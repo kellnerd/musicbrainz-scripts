6d3d9b6af19e13d4ba66b40b06fada1575cdce84
David Kellner <52860029+kellnerd@users.noreply.github.com>
2021-12-31 03:32:09 +0100

    Create a cache for Discogs to MBID mappings

---
+++ src/entityMapping.js
@@ -0,0 +1,26 @@
+import { FunctionCache } from './cache.js';
+import { buildEntityURL } from './discogs.js';
+import { getEntityForResourceURL } from './publicAPI.js';
+
+const DISCOGS_ENTITY_TYPES = {
+	artist: 'artist',
+	label: 'label',
+	release: 'release',
+	'release_group': 'master',
+};
+
+/**
+ * Maps Discogs IDs to MBIDs.
+ * @param {MB.EntityType} entityType 
+ * @param {number} discogsId 
+ */
+export async function discogsToMBID(entityType, discogsId) {
+	const discogsType = DISCOGS_ENTITY_TYPES[entityType];
+	if (!discogsType) return;
+
+	const entity = await getEntityForResourceURL(entityType, buildEntityURL(discogsType, discogsId));
+	return entity?.id;
+}
+
+export const DISCOGS2MBID = new FunctionCache('discogsToMBIDCache', discogsToMBID, (...keys) => [...keys]);
+
