2f549aa7d4bc695cbd8f00c23187b0e82b589fb6
David Kellner <52860029+kellnerd@users.noreply.github.com>
2021-12-31 03:37:28 +0100

    cache: Allow to explicitly set cache values

--- src/cache.js
+++ src/cache.js
@@ -49,6 +49,25 @@ export class FunctionCache {
 		return cacheEntry;
 	}
 
+	/**
+	 * @param {Params} params 
+	 * @param {Result} value 
+	 */
+	set(...params, value) {
+		const keys = this.keyMapper(...params);
+		let record = this.data;
+		keys.forEach((key, index) => {
+			if (index === keys.length - 1) {
+				record[key] = value;
+			} else if (record[key] === undefined) {
+				// create a new entry as empty record for all keys except the last one
+				record[key] = {};
+			}
+			// prepare to index with the next level of the key
+			record = record[key];
+		});
+	}
+
 	load() {
 		const storedData = window.localStorage.getItem(this.name);
 		if (storedData) {
