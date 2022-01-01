67f8ceb4e7064231b5b473e5ebdd259bf7ad9c92
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-01-01 21:24:44 +0100

    punctuation: Structure rules & test cases with empty lines & headers

--- src/guessUnicodePunctuation.js
+++ src/guessUnicodePunctuation.js
@@ -1,29 +1,41 @@
 import { transformInputValues } from './transformInputValues.js';
 
 export const transformationRules = [
+	/* quoted text */
 	[/(?<=[^\p{L}\d]|^)"(.+?)"(?=[^\p{L}\d]|$)/ug, '“$1”'], // double quoted text
 	[/(?<=\W|^)'(n)'(?=\W|$)/ig, '’$1’'], // special case: 'n'
 	[/(?<=[^\p{L}\d]|^)'(.+?)'(?=[^\p{L}\d]|$)/ug, '‘$1’'], // single quoted text
 	// ... which is enclosed by non-word characters or at the beginning/end of the title
 	// [^\p{L}\d] matches Unicode characters which are neither letters nor digits (\W only works with Latin letters)
+
+	/* primes */
 	[/(\d+)"/g, '$1″'], // double primes, e.g. for 12″
 	[/(\d+)'(\d+)/g, '$1′$2'], // single primes, e.g. for 3′42″ but not for 70’s
+
+	/* apostrophes */
 	[/'/g, '’'], // ... and finally the apostrophes should be remaining
+
+	/* ellipses */
 	[/(?<!\.)\.{3}(?!\.)/g, '…'], // horizontal ellipsis (but not more than three dots)
+
+	/* dashes */
 	[/ - /g, ' – '], // en dash as separator
 
-	/** hyphens for (partial) ISO 8601 dates, e.g. 1987‐07–30 or 2016-04 */
+	/* hyphens for (partial) ISO 8601 dates, e.g. 1987‐07–30 or 2016-04 */
 	[/\d{4}-\d{2}(?:-\d{2})?(?=\W|$)/g, (potentialDate) => {
 		if (Number.isNaN(Date.parse(potentialDate))) return potentialDate; // skip invalid date strings, e.g. 1989-90
 		return potentialDate.replaceAll('-', '‐');
 	}],
 
-	/* figure dashes to separate three or more groups of digits (two groups could be range) */
+	/* figure dashes: separate three or more groups of digits (two groups could be range) */
 	[/\d+(-\d+){2,}/g, (groupedDigits) => groupedDigits.replaceAll('-', '‒')],
 
 	[/(\d+)-(\d+)/g, '$1–$2'], // en dash for ranges where it means "to", e.g. 1965–1972
+
+	/* hyphens */
 	[/-/g, '‐'], // ... and finally the hyphens should be remaining
-	// difficult to find rules for: em dash (rare), minus (very rare)
+
+	/* rare cases where it is difficult to define precise rules: em dash, minus */
 ];
 
 /**
@@ -32,15 +44,19 @@ export const transformationRules = [
  * After the punctuation guessing transformation rules were applied, URLs and markup are restored.
  */
 export const transformationRulesToPreserveMarkup = [
-	// Base64 encode URLs
+	/* Base64 encode URLs */
 	[/\[(.+?)(\|.+?)?\]/g, (_match, url, label = '') => `[${btoa(url)}${label}]`], // labeled link
 	[/(?<=\/\/)(\S+)/g, (_match, path) => btoa(path)], // plain text URLs
+
 	[/'''/g, '<b>'], // bold text
 	[/''/g, '<i>'], // italic text
+
 	...transformationRules,
+
 	[/<b>/g, "'''"],
 	[/<i>/g, "''"],
-	// decode Base64 URLs
+
+	/* decode Base64 URLs */
 	[/(?<=\/\/)([A-Za-z0-9+/=]+)/g, (_match, path) => atob(path)], // plain text URLs
 	[/\[([A-Za-z0-9+/=]+)(\|.+?)?\]/g, (_match, url, label = '') => `[${atob(url)}${label}]`], // labeled link
 ];
--- test/guessUnicodePunctuation.js
+++ test/guessUnicodePunctuation.js
@@ -3,12 +3,13 @@ import { transform } from '../src/transformInputValues.js';
 import { assertFunction } from '../tools/test.js';
 
 const punctuationTestCases = [
-	// single and double quotes
+	/* single and double quotes */
 	['Are \'Friends\' Electric?', 'Are ‘Friends’ Electric?'],
 	['Axel F (from "Beverly Hills Cop" soundtrack)', 'Axel F (from “Beverly Hills Cop” soundtrack)'],
 	['"Hasta La Vista, Baby"', '“Hasta La Vista, Baby”'], // whole text in quotes
 	['One of These Days (\'French Windows\')', 'One of These Days (‘French Windows’)'], // surrounded by brackets
-	// apostrophes
+
+	/* apostrophes */
 	['I\'m Free', 'I’m Free'], // contraction
 	['Lovin\' You', 'Lovin’ You'], // omitted last letter
 	['Talkin\' \'Bout You', 'Talkin’ ’Bout You'], // omitted last letter followed by omitted first letter
@@ -18,36 +19,44 @@ const punctuationTestCases = [
 	['Rock \'n\' Roll', 'Rock ’n’ Roll'], // special case, no single quotes!
 	['Rock \'N\' Roll', 'Rock ’N’ Roll'], // case-insensitive, “Guess punctuation” used before “Guess case”
 	['Back to the 70\'s', 'Back to the 70’s'],
-	// multiple ASCII quotes and apostrophes with different meaning
+
+	/* multiple ASCII quotes and apostrophes with different meaning */
 	['Little Billy (aka \'Little Billy\'s Doing Fine\')', 'Little Billy (aka ‘Little Billy’s Doing Fine’)'],
 	['"I am 12" 7" edit of 12" remix', '“I am 12” 7″ edit of 12″ remix'],
 	['2\'59" hardcore \'Master\'s Crown Take \'Em to the Limit\' 80\'s 7" edit',
 		'2′59″ hardcore ‘Master’s Crown Take ’Em to the Limit’ 80’s 7″ edit'],
 	// failing: ['Here \'Tis (version for \'Ready, Steady, Go!\')', 'Here ’Tis (version for ‘Ready, Steady, Go!’)'],
-	// dates and date ranges
+
+	/* IS0 8601 dates and date ranges */
 	['live, 1987-07-30', 'live, 1987‐07‐30'],
 	['live, 2016-04', 'live, 2016‐04'],
 	['The Early Years: 1965-1972', 'The Early Years: 1965–1972'],
 	['advanced date range 1234-05-06-1789-10-11', 'advanced date range 1234‐05‐06–1789‐10‐11'],
 	['1989-90', '1989–90'], // second year abbreviated, not a valid date
-	// other numbers
+
+	/* other numbers */
 	['Volumes 1-5', 'Volumes 1–5'], // numeric range (en dash)
 	['ISBN 978-0-12345-678-9', 'ISBN 978‒0‒12345‒678‒9'], // figure dash to group digits
 	['2345-67-89', '2345‒67‒89'], // figure dash to group digits, not a valid date
 	// failing: ['555-1212', '555‒1212'], // figure dash, phone number with only two groups of digits (could be a range)
 	// failing: ['\'74-\'75', '’74–’75'], // would work with spaces
-	// hyphens
+
+	/* hyphens */
 	['Bron-Yr-Aur Stomp', 'Bron‐Yr‐Aur Stomp'],
-	// dashes
+
+	/* dashes */
 	['Journal de Paris - Les Pink Floyd', 'Journal de Paris – Les Pink Floyd'],
-	// ellipses
+
+	/* ellipses */
 	['Wot\'s... Uh the Deal', 'Wot’s… Uh the Deal'],
 	['...Baby One More Time', '…Baby One More Time'], // at the beginning
 	['The Gold It\'s in The...', 'The Gold It’s in The…'], // at the end
 	['Is This the World We Created...?', 'Is This the World We Created…?'], // before another punctuation symbol
-	// non-Latin scripts
+
+	/* non-Latin scripts */
 	['Όσο Και Να Σ\' Αγαπάω (Υπ\' Ευθύνη Μου)',	'Όσο Και Να Σ’ Αγαπάω (Υπ’ Ευθύνη Μου)'],
-	// ignored cases
+
+	/* ignored cases */
 	['Death on Two Legs (Dedicated to......', 'Death on Two Legs (Dedicated to......'],
 ];
 
