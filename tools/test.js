d23474f65c71e7cf8eec36c945d45f0c61bc437e
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-01-04 13:32:00 +0100

    test: Allow to assert object equality & Use template parameters

--- tools/test.js
+++ tools/test.js
@@ -2,16 +2,20 @@ import assert from 'assert';
 
 /**
  * Asserts that the given test function returns the expected output for each input of the given test cases.
- * @param {(input,...args)=>any} testFunction Function which should be applied to the input values.
- * @param {any[][]} testCases Array of test cases, each element should contain the input and its expected output.
- * @param  {...any} args Additional arguments for the test function.
+ * @template Input
+ * @template Output
+ * @template Args
+ * @param {(input:Input,...args:Args)=>Output} testFunction Function which should be applied to the input values.
+ * @param {[Input,Output][]} testCases Array of test cases, each element should contain the input and its expected output.
+ * @param  {Args} args Additional arguments for the test function.
+ * @returns {number} The number of failed test cases.
  */
 export function assertFunction(testFunction, testCases, ...args) {
 	const totalChecks = testCases.length;
 	let failures = 0;
 	testCases.forEach(([input, expectedOutput]) => {
 		try {
-			assert.strictEqual(testFunction(input, ...args), expectedOutput);
+			assert.deepStrictEqual(testFunction(input, ...args), expectedOutput);
 		} catch (assertionError) {
 			failures++;
 			console.error(assertionError.message);
