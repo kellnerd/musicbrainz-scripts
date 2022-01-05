33c5513ec7263c6e1964c7698b6131e04c9a66ba
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-01-05 18:32:44 +0100

    Clone and strip addCopyrightRelationships() for the bookmarklet
    Light version without automatic mode and caching, to reduce bookmarklet size.

--- src/bookmarklets/parseCopyrightNotice.js
+++ src/bookmarklets/parseCopyrightNotice.js
@@ -3,10 +3,41 @@
  * - Assists the user to create release-label relationships for these.
  */
 
+import { parseCopyrightNotice } from '../parseCopyrightNotice.js';
+import { LINK_TYPES } from '../relationshipData.js';
 import {
-	addCopyrightRelationships,
-	parseCopyrightNotice,
-} from '../parseCopyrightNotice.js';
+	closingDialog,
+	createAddRelationshipDialog,
+	openDialogAndTriggerAutocomplete,
+} from '../relationshipEditor.js';
+
+/**
+ * Creates and fills an "Add relationship" dialog for each piece of copyright information.
+ * Lets the user choose the appropriate target label and waits for the dialog to close before continuing with the next one.
+ * 
+ * Light version without automatic mode and caching, to reduce bookmarklet size.
+ * @param {CopyrightData[]} data List of copyright information.
+ */
+async function addCopyrightRelationships(data) {
+	for (const entry of data) {
+		const entityType = 'label';
+		const relTypes = LINK_TYPES.release[entityType];
+		const targetEntity = MB.entity({ name: entry.name, entityType });
+		for (const type of entry.types) {
+			const dialog = createAddRelationshipDialog(targetEntity);
+			const rel = dialog.relationship();
+			rel.linkTypeID(relTypes[type]);
+			rel.entity0_credit(entry.name);
+			if (entry.year) {
+				rel.begin_date.year(entry.year);
+				rel.end_date.year(entry.year);
+			}
+
+			openDialogAndTriggerAutocomplete(dialog);
+			await closingDialog(dialog);
+		}
+	}
+}
 
 const input = prompt('Copyright notice:');
 
