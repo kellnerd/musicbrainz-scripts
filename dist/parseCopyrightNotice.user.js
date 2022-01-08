09526116b474afb2489da7170ebcc206dac0b201
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-01-08 21:52:46 +0100

    copyright: Release 2022.1.8.2
    - Parse input line by line, remove only parsed lines
    - Auto-resize the credit textarea on input

--- dist/parseCopyrightNotice.user.js
+++ dist/parseCopyrightNotice.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MusicBrainz: Parse copyright notice
-// @version      2022.1.8
+// @version      2022.1.8.2
 // @namespace    https://github.com/kellnerd/musicbrainz-bookmarklets
 // @author       kellnerd
 // @description  Parses copyright notices and assists the user to create release-label relationships for these.
@@ -250,9 +250,11 @@
 	 * Automatically chooses the first search result and accepts the dialog in automatic mode.
 	 * @param {CopyrightItem[]} copyrightInfo List of copyright items.
 	 * @param {boolean} [automaticMode] Automatic mode, disabled by default.
+	 * @returns Whether a relationships has been added successfully.
 	 */
 	async function addCopyrightRelationships(copyrightInfo, automaticMode = false) {
 		const selectedRecordings = MB.relationshipEditor.UI.checkedRecordings();
+		let addedRelCount = 0;
 
 		for (const copyrightItem of copyrightInfo) {
 			const entityType = 'label';
@@ -286,6 +288,8 @@
 			}
 		}
 
+		return !!addedRelCount;
+
 		/**
 		 * @param {MB.RE.Dialog} dialog 
 		 * @param {CopyrightItem} copyrightItem 
@@ -304,6 +308,7 @@
 
 			if (targetEntity.gid || automaticMode) { // (1c) & (2b)
 				dialog.accept();
+				addedRelCount++;
 			} else { // (3b)
 				openDialogAndTriggerAutocomplete(dialog);
 				await closingDialog(dialog);
@@ -312,6 +317,7 @@
 				targetEntity = getTargetEntity(dialog);
 				if (targetEntity.gid) {
 					nameToMBIDCache.set([targetEntity.entityType, copyrightItem.name], targetEntity.gid);
+					addedRelCount++;
 				}
 			}
 			return targetEntity;
@@ -323,11 +329,44 @@
 	 */
 
 	/**
+	 * Creates a DOM element from the given HTML fragment.
+	 * @param {string} html HTML fragment.
+	 */
+	function createElement(html) {
+		const template = document.createElement('template');
+		template.innerHTML = html;
+		return template.content.firstElementChild;
+	}
+
+	/**
+	 * Creates a style element from the given CSS fragment and injects it into the document's head.
+	 * @param {string} css CSS fragment.
+	 * @param {string} userscriptName Name of the userscript, used to generate an ID for the style element.
+	 */
+	function injectStylesheet(css, userscriptName) {
+		const style = document.createElement('style');
+		if (userscriptName) {
+			style.id = [userscriptName, 'userscript-css'].join('-');
+		}
+		style.innerText = css;
+		document.head.append(style);
+	}
+
+	/**
 	 * Returns a reference to the first DOM element with the specified value of the ID attribute.
 	 * @param {string} elementId String that specifies the ID value.
 	 */
 	function dom(elementId) {
 		return document.getElementById(elementId);
+	}
+
+	/**
+	 * Returns the first element that is a descendant of node that matches selectors.
+	 * @param {string} selectors 
+	 * @param {ParentNode} node 
+	 */
+	function qs(selectors, node = document) {
+		return node.querySelector(selectors);
 	}
 
 	/**
@@ -362,6 +401,160 @@
 	}
 
 	const separator = '\n—\n';
+
+	/**
+	 * Persists the desired attribute of the given element across page loads and origins.
+	 * @param {HTMLElement} element 
+	 * @param {keyof HTMLElement} attribute 
+	 * @param {keyof HTMLElementEventMap} eventType
+	 */
+	async function persistElement(element, attribute, eventType) {
+		if (!element.id) {
+			throw new Error('Can not persist an element without ID');
+		}
+
+		const key = ['persist', element.id, attribute].join('.');
+
+		// initialize attribute
+		const persistedValue = await GM.getValue(key);
+		if (persistedValue) {
+			element[attribute] = persistedValue;
+		}
+
+		// persist attribute once the event occurs
+		element.addEventListener(eventType, () => {
+			GM.setValue(key, element[attribute]);
+		});
+	}
+
+	/**
+	 * Persists the state of the checkbox with the given ID across page loads and origins.
+	 * @param {string} id 
+	 */
+	function persistCheckbox(id) {
+		return persistElement(dom(id), 'checked', 'change');
+	}
+
+	/**
+	 * Persists the state of the collapsible details container with the given ID across page loads and origins.
+	 * @param {string} id 
+	 */
+	function persistDetails(id) {
+		return persistElement(dom(id), 'open', 'toggle');
+	}
+
+	const creditParserUI =
+`<details id="credit-parser">
+<summary>
+	<h2>Credit Parser</h2>
+</summary>
+<form>
+	<div class="row">
+		<textarea name="credit-input" id="credit-input" cols="120" rows="1" placeholder="Paste credits here…"></textarea>
+	</div>
+	<div class="row">
+		<p>Identified relationships will be added to the release and/or the matching recordings and works (only if these are selected).</p>
+	</div>
+	<div class="row">
+		<input type="checkbox" name="remove-parsed-lines" id="remove-parsed-lines" />
+		<label class="inline" for="remove-parsed-lines">Remove parsed lines</label>
+	</div>
+	<div class="row buttons">
+	</div>
+</form>
+</details>`	;
+
+	const css =
+`details#credit-parser > summary {
+	cursor: pointer;
+	display: block;
+}
+details#credit-parser > summary > h2 {
+	display: list-item;
+}
+textarea#credit-input {
+	overflow-y: hidden;
+}`	;
+
+	function buildCreditParserUI() {
+		// possibly called by multiple userscripts, do not inject the UI again
+		if (dom('credit-parser')) return;
+
+		dom('release-rels').insertAdjacentHTML('afterend', creditParserUI);
+		injectStylesheet(css, 'credit-parser');
+
+		// persist the state of the UI
+		persistDetails('credit-parser');
+		persistCheckbox('remove-parsed-lines');
+
+		// auto-resize the credit textarea on input (https://stackoverflow.com/a/25621277)
+		dom('credit-input').addEventListener('input', function () {
+			this.style.height = 'auto';
+			this.style.height = this.scrollHeight + 'px';
+		});
+	}
+
+	/**
+	 * Adds a new button with the given label and click handler to the credit parser UI.
+	 * @param {string} label 
+	 * @param {(event: MouseEvent, creditInput: HTMLTextAreaElement) => any} clickHandler 
+	 * @param {string} [description] Description of the button, shown as tooltip.
+	 */
+	function addButton(label, clickHandler, description) {
+		/** @type {HTMLTextAreaElement} */
+		const creditInput = dom('credit-input');
+
+		/** @type {HTMLButtonElement} */
+		const button = createElement(`<button type="button">${label}</button>`);
+		if (description) {
+			button.title = description;
+		}
+
+		button.addEventListener('click', (event) => clickHandler(event, creditInput));
+
+		return qs('#credit-parser .buttons').appendChild(button);
+	}
+
+	/**
+	 * Adds a new parser button with the given label and handler to the credit parser UI.
+	 * @param {string} label 
+	 * @param {(creditLine: string, event: MouseEvent) => Promise<boolean> | boolean} parser
+	 * Handler which parses the given credit line and returns whether it was successful.
+	 * @param {string} [description] Description of the button, shown as tooltip.
+	 */
+	function addParserButton(label, parser, description) {
+		/** @type {HTMLInputElement} */
+		const removeParsedLines = dom('remove-parsed-lines');
+
+		return addButton(label, async (event, creditInput) => {
+			const credits = creditInput.value.split('\n').map((line) => line.trim());
+			const parsedLines = [], skippedLines = [];
+
+			for (const line of credits) {
+				// skip empty lines, but keep them for display of skipped lines
+				if (!line) {
+					skippedLines.push(line);
+					continue;
+				}
+
+				const parserSucceeded = await parser(line, event);
+				if (parserSucceeded) {
+					parsedLines.push(line);
+				} else {
+					skippedLines.push(line);
+				}
+			}
+
+			if (parsedLines.length) {
+				addMessageToEditNote(parsedLines.join('\n'));
+			}
+
+			if (removeParsedLines.checked && skippedLines.length) {
+				creditInput.value = skippedLines.join('\n');
+				creditInput.dispatchEvent(new Event('input'));
+			}
+		}, description);
+	}
 
 	/**
 	 * Transforms the given value using the given substitution rules.
@@ -440,88 +633,17 @@
 	 * @property {string} [year] Numeric year, has to be a string with four digits, otherwise MBS complains.
 	 */
 
-	/**
-	 * Persists the desired attribute of the given element across page loads and origins.
-	 * @param {HTMLElement} element 
-	 * @param {keyof HTMLElement} attribute 
-	 * @param {keyof HTMLElementEventMap} eventType
-	 */
-	async function persistElement(element, attribute, eventType) {
-		if (!element.id) {
-			throw new Error('Can not persist an element without ID');
-		}
-
-		const key = ['persist', element.id, attribute].join('.');
-
-		// initialize attribute
-		const persistedValue = await GM.getValue(key);
-		if (persistedValue) {
-			element[attribute] = persistedValue;
-		}
-
-		// persist attribute once the event occurs
-		element.addEventListener(eventType, () => {
-			GM.setValue(key, element[attribute]);
-		});
-	}
-
-	/**
-	 * Persists the state of the checkbox with the given ID across page loads and origins.
-	 * @param {string} id 
-	 */
-	function persistCheckbox(id) {
-		return persistElement(dom(id), 'checked', 'change');
-	}
-
-	/**
-	 * Persists the state of the collapsible details container with the given ID across page loads and origins.
-	 * @param {string} id 
-	 */
-	function persistDetails(id) {
-		return persistElement(dom(id), 'open', 'toggle');
-	}
-
-	const creditParserUI =
-`<details id="credit-parser">
-<summary style="color: #EB743B; cursor: pointer;">
-	<h2 style="display: inline;">Credit Parser</h2>
-</summary>
-<form>
-	<div class="row">
-		<textarea name="credit-input" id="credit-input" cols="80" rows="10" placeholder="Paste credits here"></textarea>
-	</div>
-	<div class="row">
-		<p>Identified relationships will be added to the release and/or the matching recordings and works (only if these are selected).</p>
-	</div>
-	<div class="row">
-		<input type="checkbox" name="remove-parsed-lines" id="remove-parsed-lines" />
-		<label class="inline" for="remove-parsed-lines">Remove parsed lines</label>
-	</div>
-	<div class="row buttons">
-		<button type="button" id="parse-copyright">Parse copyright notice</button>
-	</div>
-</form>
-</details>`	;
-
 	function buildUI() {
-		dom('release-rels').insertAdjacentHTML('afterend', creditParserUI);
-
-		persistDetails('credit-parser');
-		persistCheckbox('remove-parsed-lines');
-
-		dom('parse-copyright').addEventListener('click', async (event) => {
-			/** @type {HTMLTextAreaElement} */
-			const textarea = dom('credit-input');
-			const input = textarea.value.trim();
-			if (input) {
-				const copyrightInfo = parseCopyrightNotice(input);
+		buildCreditParserUI();
+		addParserButton('Parse copyright notice', async (creditLine, event) => {
+			const copyrightInfo = parseCopyrightNotice(creditLine);
+			if (copyrightInfo.length) {
 				const automaticMode = event.altKey;
-				await addCopyrightRelationships(copyrightInfo, automaticMode);
-				addMessageToEditNote(input);
+				const result = await addCopyrightRelationships(copyrightInfo, automaticMode);
 				nameToMBIDCache.store();
-			}
-			if (dom('remove-parsed-lines').checked) {
-				textarea.value = '';
+				return result;
+			} else {
+				return false;
 			}
 		});
 	}
