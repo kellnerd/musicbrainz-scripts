ac5c2f6565e891b694f05255f17d906634193ad9
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-01-21 20:47:00 +0100

    Create a helper to poll functions until they return true

---
+++ utils/async/polling.js
@@ -0,0 +1,15 @@
+import { delay } from './delay.js';
+
+/**
+ * Periodically calls the given function until it returns `true` and resolves afterwards.
+ * @param {(...params) => boolean} pollingFunction
+ * @param {number} pollingInterval
+ */
+export function waitFor(pollingFunction, pollingInterval) {
+	return new Promise((resolve) => {
+		while (pollingFunction() === false) {
+			await delay(pollingInterval);
+		}
+		resolve();
+	});
+}
