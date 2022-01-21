6a41879c601f98bb99f905686cadf3fcb035b20f
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-01-22 00:38:46 +0100

    copyright: Use the pattern inputs as custom parser options (finally)

--- src/userscripts/parseCopyrightNotice.user.js
+++ src/userscripts/parseCopyrightNotice.user.js
@@ -1,12 +1,24 @@
 import { addCopyrightRelationships } from '../copyrightRelationships.js';
 import { addParserButton, buildCreditParserUI } from '../creditParserUI.js';
+import { dom } from '../dom.js';
 import { nameToMBIDCache } from '../nameToMBIDCache.js';
 import { parseCopyrightNotice } from '../parseCopyrightNotice.js';
+import { getPatternAsRegExp } from '../regex.js';
 
 function buildUI() {
 	buildCreditParserUI();
+
+	const terminatorInput = dom('credit-terminator');
+	const nameSeparatorInput = dom('name-separator');
+
 	addParserButton('Parse copyright notice', async (creditLine, event) => {
-		const copyrightInfo = parseCopyrightNotice(creditLine);
+		/** @type {CreditParserOptions} */
+		const customOptions = {
+			terminatorRE: getPatternAsRegExp(terminatorInput.value || '/$/'),
+			nameSeparatorRE: getPatternAsRegExp(nameSeparatorInput.value || '/$/'),
+		};
+
+		const copyrightInfo = parseCopyrightNotice(creditLine, customOptions);
 		if (copyrightInfo.length) {
 			const bypassCache = event.ctrlKey;
 			const result = await addCopyrightRelationships(copyrightInfo, bypassCache);
