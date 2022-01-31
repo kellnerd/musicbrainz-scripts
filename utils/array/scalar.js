d7bddaaf538f4ce89c6415093d89909d6fb184d2
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-01-31 23:31:49 +0100

    array: Create a new function which returns a scalar or undefined

--- utils/array/scalar.js
+++ utils/array/scalar.js
@@ -2,7 +2,22 @@
 /**
  * Converts an array with a single element into a scalar.
  * @template T
- * @param {T|T[]} maybeArray 
+ * @param {MaybeArray<T>} maybeArray 
+ * @returns A scalar or `undefined` if the conversion is not possible.
+ */
+export function toScalar(maybeArray) {
+	if (Array.isArray(maybeArray)) {
+		if (maybeArray.length === 1) return maybeArray[0];
+	} else {
+		return maybeArray;
+	}
+}
+
+/**
+ * Converts an array with a single element into a scalar.
+ * @template T
+ * @param {MaybeArray<T>} maybeArray 
+ * @returns A scalar or the input array if the conversion is not possible.
  */
 export function preferScalar(maybeArray) {
 	if (Array.isArray(maybeArray) && maybeArray.length === 1) return maybeArray[0];
@@ -12,7 +27,7 @@ export function preferScalar(maybeArray) {
 /**
  * Converts a scalar into an array with a single element.
  * @template T
- * @param {T|T[]} maybeArray 
+ * @param {MaybeArray<T>} maybeArray 
  */
 export function preferArray(maybeArray) {
 	if (!Array.isArray(maybeArray)) return [maybeArray];
