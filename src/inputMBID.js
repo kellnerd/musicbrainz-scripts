c1b41b7cfee9b497282c0bd4f551d8b380d8cbeb
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-03-17 19:22:04 +0100

    Allow an initial value to be given for MBID inputs

--- src/inputMBID.js
+++ src/inputMBID.js
@@ -7,8 +7,9 @@ import { toScalar } from '../utils/array/scalar.js';
  * It automatically validates the content on paste, loads the name of the entity and sets the MBID as a data attribute.
  * @param {string} id ID and name of the input element.
  * @param {MB.EntityType[]} [allowedEntityTypes] Entity types which are allowed for this input, defaults to all.
+ * @param {string} [initialValue] Initial value of the input element.
  */
-export function createMBIDInput(id, allowedEntityTypes) {
+export function createMBIDInput(id, allowedEntityTypes, initialValue) {
 	/** @type {HTMLInputElement} */
 	const mbidInput = document.createElement('input');
 	mbidInput.className = 'mbid';
@@ -18,17 +19,27 @@ export function createMBIDInput(id, allowedEntityTypes) {
 	const mbidAttribute = 'data-mbid';
 	const defaultEntityTypeRoute = toScalar(allowedEntityTypes) ?? 'mbid';
 
+	if (initialValue) {
+		setInputValue(initialValue);
+	}
+
 	mbidInput.addEventListener('input', async function () {
+		setInputValue(this.value.trim());
+	});
+
+	return mbidInput;
+
+	/** @param {string} entityURL */
+	async function setInputValue(entityURL) {
 		// create a complete entity identifier for an MBID only input
-		let entityURL = this.value.trim();
 		if (entityURL.match(/^[0-9a-f-]{36}$/)) {
 			entityURL = [defaultEntityTypeRoute, entityURL].join('/');
 		}
 
 		// reset previous validation results
-		this.removeAttribute(mbidAttribute);
-		this.classList.remove('error', 'success');
-		this.title = '';
+		mbidInput.removeAttribute(mbidAttribute);
+		mbidInput.classList.remove('error', 'success');
+		mbidInput.title = '';
 
 		// validate entity type and MBID
 		try {
@@ -36,19 +47,18 @@ export function createMBIDInput(id, allowedEntityTypes) {
 			if (entity) {
 				if (typeof allowedEntityTypes === 'undefined' || allowedEntityTypes.includes(entity.type)) {
 					const result = await fetchEntity(entityURL);
-					this.setAttribute(mbidAttribute, result.id);
-					this.value = result.name || result.title; // releases only have a title attribute
-					this.classList.add('success');
-					this.title = getEntityTooltip(result);
+					mbidInput.setAttribute(mbidAttribute, result.id);
+					mbidInput.value = result.name || result.title; // releases only have a title attribute
+					mbidInput.classList.add('success');
+					mbidInput.title = getEntityTooltip(result);
+					return result;
 				} else {
 					throw new Error(`Entity type '${entity.type}' is not allowed`);
 				}
 			}
 		} catch (error) {
-			this.classList.add('error');
-			this.title = error.message ?? error.statusText;
+			mbidInput.classList.add('error');
+			mbidInput.title = error.message ?? error.statusText;
 		}
-	});
-
-	return mbidInput;
+	}
 }
