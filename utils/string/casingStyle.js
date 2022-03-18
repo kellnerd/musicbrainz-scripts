fca0adb39156b4626f399bcf8574331bf27817c6
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-03-18 12:45:33 +0100

    utils: Make camelToTitleCase more robust

--- utils/string/casingStyle.js
+++ utils/string/casingStyle.js
@@ -4,9 +4,8 @@
  * @param {string} name
  */
 export function camelToTitleCase(name) {
-	return name
-		.replace(/([a-z])([A-Z])/g, '$1 $2')
-		.replace(/^./, c => c.toUpperCase());
+	return splitCamelCase(upperCaseFirstLetter(name))
+		.join(' ');
 }
 
 /**
@@ -19,4 +18,18 @@ export function slugify(string) {
 			.toLowerCase()
 			.replace(/\s+/g, '-')
 	);
+}
+
+/** @param {string} string */
+function splitCamelCase(string) {
+	return string
+		.split(/(?<=[\p{Ll}\d])(?=\p{Lu})|(?<=\p{L})(?=\d)|(?<=\p{Lu}+)(?=\p{Lu}\p{Ll}+)/u);
+	// 1. after a lower case letter or digit which is followed by an upper case letter
+	// 2. after a letter which is followed by a digit
+	// 3. after multiple upper case letters which are followed by a new word (starts with the last of these upper case letters)
+}
+
+/** @param {string} word */
+function upperCaseFirstLetter(word) {
+	return word.replace(/^./, c => c.toUpperCase());
 }
