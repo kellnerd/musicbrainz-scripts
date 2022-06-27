35b6475c93891d328af53e175b08d56145c5c41b
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-06-27 12:19:27 +0200

    utils: Implement limited concurrency for operations

--- utils/async/rateLimit.js
+++ utils/async/rateLimit.js
@@ -2,12 +2,20 @@
 
 import { delay } from './delay.js';
 
-function rateLimit1(operation, interval) {
+function rateLimitedQueue(operation, interval) {
 	let queue = Promise.resolve(); // empty queue is ready
 	return (...args) => {
 		const result = queue.then(() => operation(...args)); // queue the next operation
 		queue = queue.then(() => delay(interval)); // start the next delay
 		return result;
+	};
+}
+
+function queue(operation) {
+	let queue = Promise.resolve(); // empty queue is ready
+	return (...args) => {
+		queue = queue.then(() => operation(...args)); // queue the next operation
+		return queue; // now points to the result of the just enqueued operation
 	};
 }
 
@@ -22,12 +30,32 @@ function rateLimit1(operation, interval) {
  */
 export function rateLimit(operation, interval, requestsPerInterval = 1) {
 	if (requestsPerInterval == 1) {
-		return rateLimit1(operation, interval);
+		return rateLimitedQueue(operation, interval);
 	}
-	const queues = Array(requestsPerInterval).fill().map(() => rateLimit1(operation, interval));
+	const queues = Array(requestsPerInterval).fill().map(() => rateLimitedQueue(operation, interval));
 	let queueIndex = 0;
 	return (...args) => {
 		queueIndex = (queueIndex + 1) % requestsPerInterval; // use the next queue
-		return queues[queueIndex](...args); // return the rate-limited operation
+		return queues[queueIndex](...args); // return the result of the operation
+	};
+}
+
+/**
+ * Limits the number of simultaneous requests for the given operation.
+ * @template Params
+ * @template Result
+ * @param {(...args:Params)=>Result} operation Operation that should be limited in its use.
+ * @param {number} concurrency Maximum number of concurrent requests at any time.
+ * @returns {(...args:Params)=>Promise<Result>} Concurrency-limited version of the given operation.
+ */
+export function limit(operation, concurrency = 1) {
+	if (concurrency == 1) {
+		return queue(operation);
+	}
+	const queues = Array(concurrency).fill().map(() => queue(operation));
+	let queueIndex = 0;
+	return (...args) => {
+		queueIndex = (queueIndex + 1) % concurrency; // use the next queue
+		return queues[queueIndex](...args); // return the result of the operation
 	};
 }
