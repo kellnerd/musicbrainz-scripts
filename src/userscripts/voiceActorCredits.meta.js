808dbc7cd72ea7ebf5431ae43f1b067230068ed1
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-07-02 13:54:12 +0200

    voice-actor: Use credit parser UI & Drop jQuery

--- src/userscripts/voiceActorCredits.meta.js
+++ src/userscripts/voiceActorCredits.meta.js
@@ -3,6 +3,10 @@ const metadata = {
 	name: 'MusicBrainz: Voice actor credits',
 	author: 'kellnerd',
 	description: 'Simplifies the addition of “spoken vocals” relationships (at release level). Provides additional buttons in the relationship editor to open a pre-filled dialogue or import the credits from Discogs.',
+	grant: [
+		'GM.getValue',
+		'GM.setValue',
+	],
 	match: '*://*.musicbrainz.org/release/*/edit-relationships',
 	'run-at': 'document-idle',
 };
--- src/userscripts/voiceActorCredits.user.js
+++ src/userscripts/voiceActorCredits.user.js
@@ -1,3 +1,4 @@
+import { addButton, buildCreditParserUI } from '../creditParserUI.js';
 import { addMessageToEditNote } from '../editNote.js';
 import { buildEntityURL } from '../entity.js';
 import { discogsToMBIDCache } from '../discogs/entityMapping.js';
@@ -11,20 +12,8 @@ import { seedURLForEntity } from '../seeding.js';
 import {
 	importVoiceActorsFromDiscogs,
 } from '../voiceActorCredits.js';
-
-const addIcon = $('img', '.add-rel.btn').attr('src');
-
-const addButton =
-`<span class="add-rel btn" id="add-voice-actor-credit">
-	<img class="bottom" src="${addIcon}">
-	Add voice actor relationship
-</span>`;
-
-const importButton =
-`<span class="add-rel btn" id="import-voice-actors">
-	<img class="bottom" src="${addIcon}">
-	Import voice actors
-</span>`;
+import { createElement } from '../../utils/dom/create.js';
+import { dom, qs } from '../../utils/dom/select.js';
 
 const UI =
 `<div id="credit-import-tools">
@@ -32,48 +21,55 @@ const UI =
 	<div id="credit-import-errors" class="row no-label error"></div>
 </div>`;
 
-function buildUI() {
-	// TODO: only show buttons for certain RG types (audiobook, audio drama, spoken word) of the MB release?
-	$(addButton)
-		.on('click', (event) => createVoiceActorDialog().open(event))
-		.appendTo('#release-rels');
+// TODO: only show button for certain RG types (audiobook, audio drama, spoken word) of the MB release?
+function injectAddVoiceActorButton() {
+	const addIcon = qs('span.add-rel.btn > img')?.src;
+	const addVoiceActorButton = createElement(`
+	<span class="add-rel btn" id="add-voice-actor-credit">
+		<img class="bottom" src="${addIcon}">Add voice actor relationship
+	</span>`);
 
-	$(importButton)
-		.on('click', async () => {
-			const releaseData = await fetchEntity(window.location.href, ['release-groups', 'url-rels']);
-			const releaseURL = buildEntityURL('release', releaseData.id)
-			let discogsURL = releaseData.relations.find((rel) => rel.type === 'discogs')?.url.resource;
-
-			if (!discogsURL) {
-				discogsURL = prompt('Discogs release URL');
-			}
-
-			if (discogsURL) {
-				const result = await importVoiceActorsFromDiscogs(discogsURL);
-				addMessageToEditNote(`Imported voice actor credits from ${discogsURL}`);
-
-				// mapping suggestions
-				const newMatches = result.unmappedArtists.filter((mapping) => mapping.MBID);
-				const artistSeedNote = `Matching artist identified while importing credits from ${discogsURL} to ${releaseURL}`;
-				const messages = newMatches.map((match) => [
-					'Please add the external link',
-					`<a href="${match.externalURL}" target="_blank">${match.externalName}</a>`,
-					'to the matched entity:',
-					`<a href="${seedURLForEntity('artist', match.MBID, match.externalURL, 180, artistSeedNote)}" target="_blank">${match.name}</a>`,
-					match.comment ? `<span class="comment">(<bdi>${match.comment}</bdi>)</span>` : '',
-				].join(' '));
-
-				// import statistics
-				const importedCredits = result.mappedCredits + newMatches.length;
-				messages.unshift(`Successfully imported ${importedCredits} of ${result.totalCredits} credits, ${result.mappedCredits} of them were mapped automatically.`);
-
-				$('#credit-import-status').html(messages.map((message) => `<p>${message}</p>`).join('\n'));
-			}
-		})
-		.appendTo('#release-rels');
-
-	$(UI).appendTo('#release-rels');
+	addVoiceActorButton.addEventListener('click', (event) => createVoiceActorDialog().open(event));
+	dom('release-rels').appendChild(addVoiceActorButton);
 }
 
-discogsToMBIDCache.load();
-buildUI();
+function buildVoiceActorCreditImporterUI() {
+	discogsToMBIDCache.load();
+
+	dom('credit-parser').insertAdjacentHTML('beforeend', UI);
+
+	addButton('Import voice actors', async () => {
+		const releaseData = await fetchEntity(window.location.href, ['release-groups', 'url-rels']);
+		const releaseURL = buildEntityURL('release', releaseData.id)
+		let discogsURL = releaseData.relations.find((rel) => rel.type === 'discogs')?.url.resource;
+
+		if (!discogsURL) {
+			discogsURL = prompt('Discogs release URL');
+		}
+
+		if (discogsURL) {
+			const result = await importVoiceActorsFromDiscogs(discogsURL);
+			addMessageToEditNote(`Imported voice actor credits from ${discogsURL}`);
+
+			// mapping suggestions
+			const newMatches = result.unmappedArtists.filter((mapping) => mapping.MBID);
+			const artistSeedNote = `Matching artist identified while importing credits from ${discogsURL} to ${releaseURL}`;
+			const messages = newMatches.map((match) => [
+				'Please add the external link',
+				`<a href="${match.externalURL}" target="_blank">${match.externalName}</a>`,
+				'to the matched entity:',
+				`<a href="${seedURLForEntity('artist', match.MBID, match.externalURL, 180, artistSeedNote)}" target="_blank">${match.name}</a>`,
+				match.comment ? `<span class="comment">(<bdi>${match.comment}</bdi>)</span>` : '',
+			].join(' '));
+
+			// import statistics
+			const importedCredits = result.mappedCredits + newMatches.length;
+			messages.unshift(`Successfully imported ${importedCredits} of ${result.totalCredits} credits, ${result.mappedCredits} of them were mapped automatically.`);
+
+			dom('credit-import-status').innerHTML = messages.map((message) => `<p>${message}</p>`).join('\n');
+		}
+	}, 'Import credits from Discogs');
+}
+
+injectAddVoiceActorButton();
+buildCreditParserUI(buildVoiceActorCreditImporterUI);
