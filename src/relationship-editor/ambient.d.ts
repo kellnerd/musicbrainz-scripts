a08e7797e19a689dda14b26054cef2e884df8539
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-09-18 13:46:51 +0200

    Update ambient declarations to include the release relationship editor
    The `relationshipDialogDispatch` and `relationshipDialogState` properties
    are only available when there is an active dialog modal.

--- src/relationship-editor/ambient.d.ts
+++ src/relationship-editor/ambient.d.ts
@@ -1,19 +1,21 @@
 import type {
 	DialogActionT,
 	RelationshipEditorActionT,
+	ReleaseRelationshipEditorActionT,
 } from '../types/MBS/scripts/relationship-editor/actions';
 import type {
 	RelationshipDialogStateT,
 	RelationshipEditorStateT,
+	ReleaseRelationshipEditorStateT,
 } from '../types/MBS/scripts/relationship-editor/state';
 
 declare global {
 	namespace MB {
 		const relationshipEditor: {
-			readonly dispatch: (action: RelationshipEditorActionT) => void;
-			readonly state: RelationshipEditorStateT;
-			readonly relationshipDialogDispatch: (action: DialogActionT) => void;
-			readonly relationshipDialogState: RelationshipDialogStateT;
+			readonly dispatch: (action: RelationshipEditorActionT | ReleaseRelationshipEditorActionT) => void;
+			readonly state: RelationshipEditorStateT | ReleaseRelationshipEditorStateT;
+			readonly relationshipDialogDispatch?: (action: DialogActionT) => void;
+			readonly relationshipDialogState?: RelationshipDialogStateT;
 		};
 	}
 }
