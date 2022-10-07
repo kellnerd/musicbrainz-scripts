38e700956b652c9efcc13a35297e4da9e744e330
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-10-07 23:48:37 +0200

    utils: Add a React input value setter for textarea elements

--- utils/dom/react.js
+++ utils/dom/react.js
@@ -11,3 +11,15 @@ export function setReactInputValue(input, value) {
 	nativeInputValueSetter.call(input, value);
 	input.dispatchEvent(new Event('input', { bubbles: true }));
 }
+
+const nativeTextareaValueSetter = Object.getOwnPropertyDescriptor(HTMLTextAreaElement.prototype, 'value').set;
+
+/**
+ * Sets the value of a textarea input element which has been manipulated by React.
+ * @param {HTMLTextAreaElement} input 
+ * @param {string} value 
+ */
+export function setReactTextareaValue(input, value) {
+	nativeTextareaValueSetter.call(input, value);
+	input.dispatchEvent(new Event('input', { bubbles: true }));
+}
