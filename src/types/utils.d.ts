cea6856e32259eee7790349433ac64aae796691c
David Kellner <52860029+kellnerd@users.noreply.github.com>
2023-01-11 17:50:41 +0100

    Delete old, home-brewed type declarations for MBS internals
    I had once created these based on the available JS objects in the browser.
    Now that I have discovered and converted the official Flow typings,
    these are no longer useful. Some of them are even outdated because of
    the React relationship editor rewrite.

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -1,19 +1,3 @@
-type Getter<T> = () => T;
-
-type Setter<T> = (value: T) => void;
-
-type GetterSetter<T> = (value: T) => T;
-
-/** Recursively maps the properties of T to a combined getter and setter function with the same name. */
-type CreateGetterSetters<Record> = {
-	[Key in keyof Record]: // iterate over keys
-	Record[Key] extends object ?
-	Record[Key] extends any[] ?
-	GetterSetter<Record[Key]> : // array types
-	CreateGetterSetters<Record[Key]> : // record types (object, but not array)
-	GetterSetter<Record[Key]>; // primitive types
-};
-
 type MaybeArray<T> = T | Array<T>;
 
 type MaybePromise<T> = T | Promise<T>;
